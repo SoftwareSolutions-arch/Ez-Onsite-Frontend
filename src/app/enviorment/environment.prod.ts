5b31203e00ccf40e227f517691fd2924a9f10ae7
Umang Chopra <60170659+SoftwareSolutions-arch@users.noreply.github.com>
2025-03-07 19:11:54 +0530

    Update environment.prod.ts

--- src/app/enviorment/environment.prod.ts
+++ src/app/enviorment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://ez-onsite-backend-production.up.railway.app/api'
+    baseUrl: 'https://ez-onsite-backend-production.up.railway.app/api' //
   };
   
